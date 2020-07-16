039a87de2ab3a0d88419b5fcc766b19e65306dd6
Leith Ben Abdessalem <leithbenabdessalem@gmail.com>
2020-07-16 13:35:26 +0200

    E-Cave prefabs and base scripts

---
+++ Assets/E-CAVE/Scripts/CaveManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Kinect = Windows.Kinect;
+
+
+public class POV : IComparable<POV>
+{
+    public GameObject screen, camera;
+
+    public POV(GameObject newScreen, GameObject newCamera)
+    {
+        screen = newScreen;
+        camera = newCamera;
+    }
+
+    //This method is required by the IComparable
+    //interface. 
+    public int CompareTo(POV other)
+    {
+        if (other == null)
+        {
+            return 1;
+        }
+
+        //Return the difference in power.
+        return power - other.power;
+    }
+}
+
+public class CaveManager : MonoBehaviour
+{
+    public GameObject cameraPrefab, screenContainer;
+    private Transform head, bodyView;
+    private List<GameObject> screens = new List<GameObject>();
+    private List<GameObject> cameras =
+
+
+
+    private void Awake()
+    {
+        foreach (Transform screen in screenContainer.transform)
+        {
+            screens.Add(screen.gameObject);
+
+        }
+
+
+        cam = Instantiate(cameraPrefab, this.transform).GetComponent<Camera>();
+        cam.GetComponent<ProjMatrix>().projectionScreen = screen.gameObject;
+        cam.transform.parent = this.transform;
+        cam.cullingMask = ~(1 << 9);
+        cam.aspect = screenResolution.x / screenResolution.y;
+
+        bodyView = this.transform.Find("BodyView");
+
+
+    }
+
+    private void Update()
+    {
+        if (!head)
+        {
+            FindHead();
+            if (!head) return;
+        }
+
+        UpdateCamera();
+
+    }
+
+    private void FindHead()
+    {
+        var body_ = bodyView.Find("Body");
+        if (!body_) return;
+        var head_ = body_.Find("Head");
+        if (!head_) return;
+
+        head = head_;
+    }
+
+    private void UpdateCamera()
+    {
+        //UpdateCameraFOV();
+        UpdateCameraPosition();
+        UpdateCameraRotation();
+    }
+
+    private void UpdateCameraPosition()
+    {
+        cam.transform.position = head.transform.position;
+    }
+
+    private void UpdateCameraRotation()
+    {
+        cam.transform.LookAt(screen, Vector3.up);
+    }
+}
